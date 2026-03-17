98c9f512add628e4abb311927ebb83ba08ed9c82
manueljose199-spec <manueljoseemma199@gmail.com>
2026-03-16 19:51:01 -0600

    feat:se integro metodos para interactuar con el arreglo y en el html se integraron las capturas

--- ListaDeObjetos/main.js
+++ ListaDeObjetos/main.js
@@ -7,3 +7,15 @@ const productos = [
     { nombre: "Guantes", precio: 20, categoria: "Ropa" },
     { nombre: "iPod", precio: 99, categoria: "Electrónica" },
 ];
+
+// Encadenamiento de metodos 
+const resultado = productos 
+// Filter
+.filter(producto => producto.precio < 100) // productos que cuesten menos de 100 
+// Sort
+.sort((a,b) => a.nombre.localeCompare(b.nombre)) // localeCompare compara strings correctamente y ordenamos alfabeticamente
+// Map 
+.map(producto => producto.nombre); // Devuelve solo el nombre del producto
+
+console.log('Resultados de usar los metodos Filter, Sort y Map')
+console.log(resultado)
