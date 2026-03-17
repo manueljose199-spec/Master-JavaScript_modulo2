ea6940f8e838f615522f45ac13a838f4ad194e64
manueljose199-spec <manueljoseemma199@gmail.com>
2026-03-16 19:42:47 -0600

    feat:se integro arreglo con 5 productos con nombre,precio,categoria, y tipo

---
+++ ListaDeObjetos/main.js
@@ -0,0 +1,9 @@
+// Arreglo de objetos con al menos 5 productos, cada uno con las propiedades nombre, precio y categoría.
+
+const productos = [
+    { nombre: "Abrigo", precio: 1500, categoria: "Ropa" },
+    { nombre: "Laptop", precio: 800, categoria: "Electrónica" },
+    { nombre: "Lapices", precio: 50, categoria: "Educación" },
+    { nombre: "Guantes", precio: 20, categoria: "Ropa" },
+    { nombre: "iPod", precio: 99, categoria: "Electrónica" },
+];
